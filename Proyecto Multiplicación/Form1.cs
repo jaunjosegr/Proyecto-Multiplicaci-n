cb89da5b5b4a88b54ade86c9cab437379338685d
jg602 <jg602@JUANJOS>
2022-09-05 09:06:33 -0700

    Agregar archivos de proyecto.

---
+++ Proyecto Multiplicación/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Multiplicación
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clmul SS = new clmul();
+
+            SS.N1 = int.Parse(textBox1.Text);
+            SS.N2 = int.Parse(textBox2.Text);
+
+            textBox3.Text = SS.OP().ToString();
+
+            clmul pp = new clmul(int.Parse(textBox1.Text),
+            int.Parse(textBox2.Text)); textBox3.Text = pp.OP()
+            .ToString();
+
+        }
+    }
+}
---
+++ Proyecto Multiplicación/clmul.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Multiplicación
+{
+    internal class clmul
+    {
+        private int n1;
+        private int n2;
+
+        public clmul(int n1, int n2)
+        {
+            this.n1 = n1;
+            this.n2 = n2;
+        }
+
+        public int N1 { get => n1; set => n1 = value; }
+        public int N2 { get => n2; set => n2 = value; }
+
+        public int OP()
+        {
+            return N1 * N2;
+        }
+
+        public clmul()
+        {
+
+        }
+    }
+}
